05a1d9ff89245a17edf13aa7c4d36713af5dc0da
Hồ Bá Quỳnh <84196934+hobaquynh2002@users.noreply.github.com>
2022-03-30 19:43:33 +0700

    Update Selection-sort.c

--- Selection-sort.c
+++ Selection-sort.c
@@ -9,10 +9,11 @@ void selectionSort(int arr[], int n){
 	int i, j, min;
 	for (i = 0; i < n-1; i++){
 		min = i;
-		for (j = i+1; j < n; j++)
+		for (j = i+1; j < n; j++){
 		// if (arr[j] < arr[min])
 		// 	min = j;
-        min=(arr[j] > arr[min])?min:j;    
+        	min=(arr[j] > arr[min])?min:j;
+		}	
 		swap(&arr[min], &arr[i]);
 	}
 }
